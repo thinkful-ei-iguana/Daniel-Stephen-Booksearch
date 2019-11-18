0e5d791fa6cf7d94a42070792768bf3613533648
stephenschab <sschab689@gmail.com>
2019-11-18 10:01:05 -0700

    added handle book type and print filter methods to app.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import './App.css';
 import Search from './Search';
 import FilterBar from './FilterBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Google Book Search</h1>
-      </header>
-        <main>
-          <Search />
-          <FilterBar />
-        </main>  
-    </div>
+class App extends React.Component {
+  state = {
+    "print filter": null,
+    "book type": "all",
+  }
+
+  handlePrintFilter = (value) => {
+    this.setState({ "print filter": value })
+  }
+
+  handleBookType = (value) => {
+    this.setState({ "book type": value })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Google Book Search</h1>
+        </header>
+          <main>
+            <Search />
+            <FilterBar />
+          </main>  
+      </div>
   );
+  }
 }
 
 export default App;
--- src/ResultsList.js
+++ src/ResultsList.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ResultsList(props) {
   return (
     <div className='results-list'>
-      
+      {props.data}
     </div>
   )
 }
